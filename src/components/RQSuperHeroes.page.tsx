3e7609740a6629fe8229639767d1336f10ae6408
KimJoonSeo <junjun901225@gmail.com>
2023-09-26 00:03:28 +0900

    Feat: Add Error Handling

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,19 +1,22 @@
 import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 
 interface Hero {
     id: number,
     name: string,
     alterEgo: string,
 }
-const fetchSuperHeroes = (): Promise<Hero[]> => {
-    return axios.get('http://localhost:4000/superheroes')
-                .then(res => res.data);
+const fetchSuperHeroes = async (): Promise<Hero[]> => {
+    const res = await axios.get('http://localhost:4000/superheroes');
+    return res.data;
 }
 export const RQSuperHeroesPage = () => {
-    const {isLoading, data} = useQuery(['super-heroes'], fetchSuperHeroes);
+    const {isLoading, data, isError, error} = useQuery<Hero[], AxiosError>(['super-heroes'], fetchSuperHeroes);
     if(isLoading) {
         return <h2>Loading...</h2>
+    }
+    if(isError) {
+        return <h2>{error?.message}</h2>
     }
     return (
         <>
--- src/components/SuperHeroes.page.tsx
+++ src/components/SuperHeroes.page.tsx
@@ -2,18 +2,25 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export const SuperHeroesPage = () => {
-    const [isLoading, setIsLoading] = useState(true)
-    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState('');
 
     useEffect(() => {
         axios.get('http://localhost:4000/superheroes').then(res => {
-            setData(res.data)
-            setIsLoading(false)
+            setData(res.data);
+            setIsLoading(false);
+        }).catch(error => {
+            setError(error.message);
+            setIsLoading(false);
         })
     }, [])
 
     if (isLoading) {
         return <h2>Loading...</h2>
+    }
+    if (error) {
+        return <h2>{error}</h2>
     }
 
     return (
