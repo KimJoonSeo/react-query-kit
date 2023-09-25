71958464ae6f4076237a70e62e1f2bc09b1553b5
KimJoonSeo <junjun901225@gmail.com>
2023-09-25 21:44:09 +0900

    Init: Start project

---
+++ src/components/SuperHeroes.page.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+interface Hero {
+    id: number,
+    name: string,
+    alterEgo: string,
+}
+export const SuperHeroesPage = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/superheroes').then(res => {
+            setData(res.data)
+            setIsLoading(false)
+        })
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    return (
+        <>
+            <h2>Super Heroes Page</h2>
+            {data.map(({name}) => {
+                return <div>{name}</div>
+            })}
+        </>
+    )
+}
