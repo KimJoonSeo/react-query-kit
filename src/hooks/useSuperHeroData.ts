c437d954a90a239971fb9624141adf8f71daa11f
KimJoonSeo <junjun901225@gmail.com>
2023-09-30 21:22:50 +0900

    Feat: add a initialData option to useSuperHeroData hook not to show loading bar

--- src/hooks/useSuperHeroData.ts
+++ src/hooks/useSuperHeroData.ts
@@ -1,12 +1,21 @@
 import {Hero} from "../components/RQSuperHeroes.page";
 import axios, {AxiosError} from "axios";
-import {useQuery} from "@tanstack/react-query";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 
 export const fetchSuperHero = async (heroId: string): Promise<Hero> => {
     const res = await axios.get(`http://localhost:4000/superheroes/${heroId}`);
     return res.data;
 }
 const useSuperHeroData = (heroId: string | undefined) => {
-    return useQuery<Hero, AxiosError>(['super-hero', heroId], () => fetchSuperHero(heroId === undefined ? '0' : heroId));
+    const id = heroId === undefined ? '0' : heroId;
+    const queryClient = useQueryClient();
+    const data: Hero[] | undefined = queryClient.getQueryData(['super-heroes']);
+    const initialData = data?.find(hero => hero.id === parseInt(id));
+    useQuery({
+        queryKey: ['super-hero', id],
+        queryFn: () => fetchSuperHero(id),
+        initialData: initialData
+    })
+    return useQuery<Hero, AxiosError>(['super-hero', id], () => fetchSuperHero(id));
 }
 export default useSuperHeroData;
