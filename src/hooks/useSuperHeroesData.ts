9915572fcf25e1270a74b6dc239779958b3e8dec
KimJoonSeo <junjun901225@gmail.com>
2023-10-01 18:56:14 +0900

    Feat: add an abstraction to axios request for common logics
    - authorization
    - error handling

--- src/hooks/useSuperHeroesData.ts
+++ src/hooks/useSuperHeroesData.ts
@@ -1,15 +1,17 @@
 import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import axios, {AxiosError} from "axios";
 import {Hero} from "../components/RQSuperHeroes.page";
+import {request} from "../utils/axios-utils";
 
 export const fetchSuperHeroes = async (): Promise<Hero[]> => {
-    const res = await axios.get('http://localhost:4000/superheroes');
+    const res = await request({url: '/superheroes'});
     return res.data;
 }
 
 const addSuperHero = async (hero: Hero): Promise<Hero> => {
-    const rs = await axios.post('http://localhost:4000/superheroes1', hero);
-    return rs.data;
+    const res = await request({url: '/superheroes', method: 'post', data: hero});
+        // await axios.post('http://localhost:4000/superheroes1', hero);
+    return res.data;
 }
 const useSuperHeroesData = () => useQuery<Hero[], AxiosError>(
     ['super-heroes'],
